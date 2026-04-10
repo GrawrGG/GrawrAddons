0048b5952463960003ee6a6cfc85185185c9a6b6
Grawr <grawrgaming@gmail.com>
2026-04-10 20:35:06 +1200

    Add ServerName scaffold

---
+++ ServerName/ServerName.lua
@@ -0,0 +1,9 @@
+local AddonName, ns = ...
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("ADDON_LOADED")
+frame:SetScript("OnEvent", function(self, event, ...)
+    if event == "ADDON_LOADED" and ... == "ServerName" then
+        -- init
+    end
+end)
